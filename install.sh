fed8d71c2955625274dc6d868280703237f9b5b3
wb <wb@wbhome.wb.lan>
2014-01-04 17:14:16 +0100

    Moved config file into install script

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-# Initialize our own variables:
+
 rb_version=""
+
+read -r -d '' LOOPER_CONF <<'EOF'
+[Plugin]
+Loader=##LOADER##
+Module=looper
+IAge=2
+Name=Looper
+Description=Loop part of the song
+Authors=Ivan Augustinović <augustinovic.ivan@gmail.com>
+Copyright=Copyright © 2013 Ivan Augustinović
+Website=https://github.com/kingd/looper
+EOF
 
 show_help()
 {
     echo "Usage: bash install.sh -v [rb2|rb3]"
 }
 
-OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
+OPTIND=1 # Reset is necessary if getopts was used previously in the script.
 while getopts "v:" opt; do
     case "$opt" in
         v)
@@ -21,21 +33,21 @@ while getopts "v:" opt; do
 done
 shift $((OPTIND-1)) # Shift off the options and optional --.
 
-SCRIPT_PATH=${0%`basename "$0"`}
-
 if [ "$rb_version" = "rb2" ]; then
-    CONF_PATH="${SCRIPT_PATH}conf/plugin2"
+    LOADER="python"
 elif [ "$rb_version" = "rb3" ]; then
-    CONF_PATH="${SCRIPT_PATH}conf/plugin3"
+    LOADER="python3"
 else
     show_help
     exit
 fi
 
+LOOPER_CONF=${LOOPER_CONF/"##LOADER##"/$LOADER}
+SCRIPT_PATH=${0%`basename "$0"`}
 PLUGINS_PATH="/home/${USER}/.local/share/rhythmbox/plugins/"
 PLUGIN_PATH="${PLUGINS_PATH}looper"
 
 rm -rf $PLUGIN_PATH
 mkdir -p $PLUGINS_PATH
 cp -r "${SCRIPT_PATH}looper" "$PLUGIN_PATH"
-cp "${CONF_PATH}" "$PLUGIN_PATH/.plugin"
+echo "$LOOPER_CONF" > "$PLUGIN_PATH/.plugin"
